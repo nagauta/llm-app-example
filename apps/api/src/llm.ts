a8be0ff675a1e8b4803dc13daa94a55909e0f916
mie035 <hello@michitoshiyano.com>
2024-08-31 20:41:23 +0900

    chore: add exmple

---
+++ apps/api/src/llm.ts
@@ -0,0 +1,39 @@
+import { promises as fs } from "fs";
+import { Document, HuggingFaceEmbedding, Ollama, Settings, VectorStoreIndex } from "llamaindex";
+
+console.log(`go...`);
+Settings.llm = new Ollama({
+    model: "llama3.1",
+  });
+
+  Settings.embedModel = new HuggingFaceEmbedding({
+    modelType: "BAAI/bge-small-en-v1.5",
+    quantized: false,
+  });
+
+export async function llm() {
+    // Load essay from abramov.txt in Node
+    const path = "./rc/chiikawa.txt";
+
+    const essay = await fs.readFile(path, "utf-8");
+  
+    // Create Document object with essay
+    const document = new Document({ text: essay, id_: path });
+  
+    // Split text and create embeddings. Store them in a VectorStoreIndex
+    const index = await VectorStoreIndex.fromDocuments([document]);
+  
+    // Query the index
+    const queryEngine = index.asQueryEngine();
+  
+    console.log(`response: start...`);
+    const response = await queryEngine.query({
+      query: "What did the author do in college?",
+    });
+    console.log(`response: ${JSON.stringify(response)}`);
+  
+    // Output response
+    console.log(response.toString());
+  }
+
+llm().then(() => {console.log(`end`); });
